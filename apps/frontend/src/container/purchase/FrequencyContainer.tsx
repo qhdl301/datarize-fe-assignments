d29156f61077f6fa9de2908f629de6f19b204773
qhdl301 <>
2024-10-25 22:19:41 +0900

    refactor(purchase): 구매 빈도 관련 error 처리

--- apps/frontend/src/container/purchase/FrequencyContainer.tsx
+++ apps/frontend/src/container/purchase/FrequencyContainer.tsx
@@ -5,14 +5,14 @@ import { DateSelector, FrequencyChart } from '../../components'
 const PurchaseFrequencyContainer: FC = () => {
   const [startDate, setStartDate] = useState('2024-07-01')
   const [endDate, setEndDate] = useState('2024-07-31')
-  const [error, setError] = useState('')
+  const [validation, setValidation] = useState('')
 
   const validateDates = (start: string, end: string) => {
     if (new Date(start) > new Date(end)) {
-      setError('시작 날짜는 종료 날짜보다 이전이어야 합니다.')
+      setValidation('시작 날짜는 종료 날짜보다 이전이어야 합니다.')
       return false
     }
-    setError('') // 에러 메시지 초기화
+    setValidation('') // validation 메시지 초기화
     return true
   }
 
@@ -26,7 +26,7 @@ const PurchaseFrequencyContainer: FC = () => {
     validateDates(startDate, date)
   }
 
-  const { data, isLoading } = usePurchaseFrequency(startDate, endDate)
+  const { data, isLoading, isError } = usePurchaseFrequency(startDate, endDate)
 
   return (
     <>
@@ -37,9 +37,12 @@ const PurchaseFrequencyContainer: FC = () => {
         setStartDate={handleStartDateChange}
         setEndDate={handleEndDateChange}
       />
-      {error && <div className="text-red-500">{error}</div>}
+      {/** 달력 validation 노출 영역 */}
+      {validation && <div className="text-red-500">{validation}</div>}
+      {/** 차트 오류 영역 */}
+      {isError && <div className="text-red-500">데이터를 불러오는 중 차트 오류가 발생했습니다.</div>}
       {/** 차트 노출 영역 */}
-      {isLoading ? <div>Loading...</div> : <FrequencyChart chartList={data || []} />}
+      {isLoading && !isError ? <div>Loading...</div> : <FrequencyChart chartList={data || []} />}
     </>
   )
 }
